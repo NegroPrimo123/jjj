529b57e6a32584df8d5dfae70a1b7ecf87f361b3
NegroPrimo123 <pereverzevm44@gmail.com>
2025-09-07 11:27:15 +0300

    Add files via upload

---
+++ DataAccess/Models/Course.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Models;
+
+/// <summary>
+/// Таблица учебных дисциплин (курсов)
+/// </summary>
+public partial class Course
+{
+    /// <summary>
+    /// Уникальный идентификатор курса (AI)
+    /// </summary>
+    public int CourseId { get; set; }
+
+    public string CourseName { get; set; } = null!;
+
+    /// <summary>
+    /// Внешний ключ к таблице преподавателей (кто ведет курс)
+    /// </summary>
+    public int TeacherId { get; set; }
+
+    public virtual ICollection<Project> Projects { get; set; } = new List<Project>();
+
+    public virtual Teacher Teacher { get; set; } = null!;
+}
---
+++ DataAccess/Models/Studentteam.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess.Models;
+
+/// <summary>
+/// Таблица для связи многие-ко-многим между Студентами и Командами
+/// </summary>
+public partial class Studentteam
+{
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Внешний ключ к таблице студентов
+    /// </summary>
+    public int StudentId { get; set; }
+
+    /// <summary>
+    /// Внешний ключ к таблице команд
+    /// </summary>
+    public int TeamId { get; set; }
+
+    public virtual Student Student { get; set; } = null!;
+
+    public virtual Projectteam Team { get; set; } = null!;
+}
